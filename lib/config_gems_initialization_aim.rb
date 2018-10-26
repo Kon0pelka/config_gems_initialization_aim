f69ee37465ffda8fb7b29136eeebb423e16ab73f
Maxim Kon0pelka <tkach32max@gmail.com>
2018-10-26 09:32:05 +0300

    Init gem

---
+++ lib/config_gems_initialization_aim.rb
@@ -0,0 +1,5 @@
+require "config_gems_initialization_aim/version"
+
+module ConfigGemsInitializationAim
+  # Your code goes here...
+end
---
+++ lib/config_gems_initialization_aim/version.rb
@@ -0,0 +1,3 @@
+module ConfigGemsInitializationAim
+  VERSION = "0.1.0"
+end

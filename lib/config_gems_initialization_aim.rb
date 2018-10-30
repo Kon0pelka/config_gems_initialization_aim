95ad0e2d33388e46beb49a9e177f1cc04372a07a
Maxim Kon0pelka <tkach32max@gmail.com>
2018-10-30 09:36:13 +0300

    Fix

--- lib/config_gems_initialization_aim.rb
+++ lib/config_gems_initialization_aim.rb
@@ -1,5 +1,8 @@
 require "config_gems_initialization_aim/version"
 require "config_gems_initialization_aim/railtie" if defined?(Rails)
+require "overcommit"
+require "rubocop"
+require "reek"
 
 module ConfigGemsInitializationAim
   # Your code goes here...
